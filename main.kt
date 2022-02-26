0a83e42a0f0b52633b5a1a8b81963a29ab40f515
Shamimgardobuya <shamimobuya@gmail.com>
2022-02-26 23:37:45 +0300

    first good kotlin commit

---
+++ main.kt
@@ -0,0 +1,8 @@
+fun main(){
+    var name= "akirachix"
+    char1= (name[0])
+    char2= (name[2])
+    char3= (name[3])
+    var result= "$char1 $char2 $char3"
+    print(result)
+}
---
+++ src/main.kt
@@ -0,0 +1,27 @@
+fun main() {
+    var name= "akirachix"
+    var char1= (name[0])
+    var char2= (name[2])
+    var char3= (name[3])
+    var result= "$char1$char2$char3"
+    println(result)
+
+    var x ="Conslate"
+    var y = 36
+    println("Hi, my name is $x and I am $y years old")
+
+    var myName= "Conslate"
+    var length= myName.length
+    println(length)
+
+
+    var name1 ="Conslate"
+    var name2 ="Verite"
+    if (name1==name2)
+        println("That’s me!")
+    else (
+            println("I don’t know who that is")
+
+    )
+}
+
